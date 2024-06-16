ee43764c364727ac209d21a311c9162ac763101b
mkljngd <37976753+mkljngd@users.noreply.github.com>
2024-06-17 02:07:08 +0530

    Improved Project Controller test cases and Project service implementation

--- code/pro-manager/src/main/java/com/cs673olsum24/promanager/serviceImpl/ProjectServiceImplementation.java
+++ code/pro-manager/src/main/java/com/cs673olsum24/promanager/serviceImpl/ProjectServiceImplementation.java
@@ -1,6 +1,6 @@
 package com.cs673olsum24.promanager.serviceImpl;
 
-import com.cs673olsum24.promanager.dao.*;
+import com.cs673olsum24.promanager.dao.ProjectDAO;
 import com.cs673olsum24.promanager.entity.Projects;
 import com.cs673olsum24.promanager.service.ProjectServices;
 import com.cs673olsum24.promanager.utils.JsonUtils;
@@ -8,7 +8,6 @@ import com.cs673olsum24.promanager.utils.ProjectUtils;
 import com.fasterxml.jackson.core.JsonProcessingException;
 import jakarta.servlet.http.HttpServletRequest;
 import java.util.HashMap;
-import java.util.Iterator;
 import java.util.List;
 import java.util.Map;
 import org.json.simple.JSONArray;
@@ -19,193 +18,109 @@ import org.springframework.stereotype.Service;
 @Service
 public class ProjectServiceImplementation implements ProjectServices {
 
+  @Autowired private ProjectDAO projectDAO;
 
-	@Autowired
-	private ProjectDAO projectDAO;
+  @Override
+  public Map<String, Object> getAllProjects() throws JsonProcessingException {
+    Map<String, Object> response = new HashMap<>();
+    List<Object[]> data = projectDAO.findAllProjects();
+    response.put("projects", data);
+    return response;
+  }
 
-	public Map<String, Object> getAllProjects() throws JsonProcessingException {
-		Map<String, Object> map = new HashMap<>();
-		List<Object []> data = this.projectDAO.findAllProjects();
-		map.put("projects", data);
-		return map;
-	}
-	
-	public Map<String, Object> getIdWiseProject(String id) throws JsonProcessingException {
-		Map<String, Object> map = new HashMap<>();
-		List<Object []> data = this.projectDAO.findIdWiseProjects(id);
-		map.put("project", data);
-		return map;
-	}
+  @Override
+  public Map<String, Object> getIdWiseProject(String id) throws JsonProcessingException {
+    Map<String, Object> response = new HashMap<>();
+    List<Object[]> data = projectDAO.findIdWiseProjects(id);
+    response.put("project", data);
+    return response;
+  }
 
+  @Override
+  @SuppressWarnings("unchecked")
+  public Map<String, Object> addProject(HttpServletRequest request, Map<String, Object> payload) {
+    JSONObject data = JsonUtils.convertPayload(payload);
+    JSONArray projects = (JSONArray) data.get("projects");
 
-	
-	
-	
-	@SuppressWarnings("unchecked")
-	public Map<String, Object> addProject(HttpServletRequest request, Map<String, Object> payload) {
-
-		JSONObject data = JsonUtils.convertPayload(payload);
-		JSONArray projects = (JSONArray) data.get("projects");
-		
-
-		Iterator<JSONObject> result = projects.iterator();
-		while (result.hasNext()) {
-			JSONObject each = (JSONObject) result.next();
-			
-			
-//			Object createdOnObj = each.getOrDefault("updated_on", 0L);
-//			Long createdOn;
-//			if (createdOnObj instanceof Integer) {
-//				createdOn = ((Integer) createdOnObj).longValue();
-//			} else {
-//				createdOn = (Long) createdOnObj;
-//			}
-			Long createdOn = (Long) each.getOrDefault("created_on", 0);
-			
-			
-			System.out.println("here update");
-			
-			Long updatedOn = (Long) each.getOrDefault("updated_on", 0);
-//			Object updatedOnObj = each.getOrDefault("updated_on", 0L);
-//			Long updatedOn;
-//			if (updatedOnObj instanceof Integer) {
-//				updatedOn = ((Integer) updatedOnObj).longValue();
-//			} else {
-//				updatedOn = (Long) updatedOnObj;
-//			}
-			
-			
-			System.out.println("owner update");
-			
-			System.out.println(each.getOrDefault("project_id", "NA"));
-			
-			
-//			int owner_id = (int) each.getOrDefault("owner_id", 1);
-//			
-			Object ownerIdObj = each.getOrDefault("owner_id", 1);
-			int owner_id;
-
-			if (ownerIdObj instanceof Long) {
-			    owner_id = ((Long) ownerIdObj).intValue();
-			} else if (ownerIdObj instanceof Integer) {
-			    owner_id = (Integer) ownerIdObj;
-			} else {
-			    owner_id = 1; // Default value
-			}
-			
-			System.out.println("owner update complete");
-			
-
-			Projects p = new Projects();
-
-			p.setProjectid((String) each.getOrDefault("project_id", "NA"));
-			p.setProjectname((String) each.getOrDefault("projectname", "NA"));
-			
-			p.setDescription(each.getOrDefault("description", "NA").toString());
-			p.setStatus((String) each.getOrDefault("status", "OPEN"));
-			p.setType((String) each.getOrDefault("type", "NA"));
-			p.setActive((boolean)each.getOrDefault("active","TRUE"));
-
-			p.setCreated_on( createdOn);
-			p.setUpdated_on(updatedOn);
-			p.setOwner_id(owner_id);
-			
-			
-
-			try {
-				this.projectDAO.addProjects(p);
-			} catch (Exception e) {
-				System.out.println(e);
-			}
-		}
-
-		Map<String, Object> map = new HashMap<>();
-		map.put("Response", "OK");
-
-		return map;
-	}
-
-	public Map<String, Object> deleteProject(String id)
-	{
-		Map<String, Object> map2 = new HashMap<>();
-
-		this.projectDAO.deleteProject(id);
-
-		map2.put("Response","Success");
-		return map2;
-
-	}
-
-
-
-
-	//	@SuppressWarnings("unchecked")
-	//	public Map<String, Object> safeDeleteProject(JSONObject each)
-	//	{
-	//		Map<String, Object> map = new HashMap<>();		
-	//		Projects p = new Projects();	
-	//		
-	//		p.setProjectid((String) each.getOrDefault("projectid", "NA"));
-	//		
-	//
-	//		try {
-	//			this.projectDAO.safeDeleteProject(p);
-	//			map.put("Response", "Success");
-	//		}
-	//		catch(Exception e)		
-	//		{
-	//			map.put("Response", "Failed");
-	//		}
-	//
-	//    	return map;
-	//	}
-
-
-		@SuppressWarnings("unchecked")
-		public Map<String, Object> editProject(JSONObject each)
-		{
-			Map<String, Object> map = new HashMap<>();		
-			Projects p = new Projects();
-			System.out.println(each);
-			Object updatedOnObj = each.getOrDefault("updated_on", 0L);
-			Long updatedOn;
-			if (updatedOnObj instanceof Integer) {
-				updatedOn = ((Integer) updatedOnObj).longValue();
-			} else {
-				updatedOn = (Long) updatedOnObj;
-			}
-
-			int owner_id = (int) each.getOrDefault("owner_id", 1);
-			
-
-			p.setProjectid(
-			ProjectUtils.safelyGetString(each, "project_id", ProjectUtils.DEFAULT_PROJECT_ID));
-			p.setProjectname(
-			ProjectUtils.safelyGetString(each, "projectname", ProjectUtils.DEFAULT_PROJECT_NAME));
-			
-			p.setUpdated_on(updatedOn);		
-			p.setDescription(
-			ProjectUtils.safelyGetString(each, "description", ProjectUtils.DEFAULT_DESCRIPTION));			
-			p.setStatus(ProjectUtils.safelyGetString(each, "status", ProjectUtils.DEFAULT_STATUS));
-			p.setType(ProjectUtils.safelyGetString(each, "type", ProjectUtils.DEFAULT_TYPE));
-			p.setOwner_id(owner_id);
-			
-			try {
-				this.projectDAO.editProject(p);
-				map.put("Response", "Success");
-			}
-			catch(Exception e)		
-			{
-				map.put("Response", "Failed");
-			}
-
-    try {
-      this.projectDAO.editProject(p);
-      map.put("Response", "Success");
-    } catch (Exception e) {
-      map.put("Response", "Failed");
+    for (Object obj : projects) {
+      JSONObject each = (JSONObject) obj;
+      Projects project = parseProject(each);
+      try {
+        projectDAO.addProjects(project);
+      } catch (Exception e) {
+        e.printStackTrace();
+      }
     }
 
-    return map;
+    Map<String, Object> response = new HashMap<>();
+    response.put("Response", "OK");
+    return response;
+  }
+
+  @Override
+  public Map<String, Object> deleteProject(String id) {
+    Map<String, Object> response = new HashMap<>();
+    try {
+      projectDAO.deleteProject(id);
+      response.put("Response", "Success");
+    } catch (Exception e) {
+      e.printStackTrace();
+      response.put("Response", "Failed");
+    }
+    return response;
+  }
+
+  @Override
+  @SuppressWarnings("unchecked")
+  public Map<String, Object> editProject(JSONObject each) {
+    Map<String, Object> response = new HashMap<>();
+    Projects project = parseProject(each);
+
+    try {
+      projectDAO.editProject(project);
+      response.put("Response", "Success");
+    } catch (Exception e) {
+      e.printStackTrace();
+      response.put("Response", "Failed");
+    }
+    return response;
+  }
+
+  private Projects parseProject(JSONObject each) {
+    Projects project = new Projects();
+
+    project.setProjectid(
+        ProjectUtils.safelyGetString(each, "project_id", ProjectUtils.DEFAULT_PROJECT_ID));
+    project.setProjectname(
+        ProjectUtils.safelyGetString(each, "projectname", ProjectUtils.DEFAULT_PROJECT_NAME));
+    project.setDescription(
+        ProjectUtils.safelyGetString(each, "description", ProjectUtils.DEFAULT_DESCRIPTION));
+    project.setStatus(ProjectUtils.safelyGetString(each, "status", ProjectUtils.DEFAULT_STATUS));
+    project.setType(ProjectUtils.safelyGetString(each, "type", ProjectUtils.DEFAULT_TYPE));
+    project.setActive((Boolean) each.getOrDefault("active", true));
+
+    Long createdOn =
+        ProjectUtils.safelyConvertToLong(
+            each.getOrDefault("created_on", ProjectUtils.DEFAULT_DATE));
+    Long updatedOn =
+        ProjectUtils.safelyConvertToLong(
+            each.getOrDefault("updated_on", ProjectUtils.DEFAULT_DATE));
+
+    project.setCreated_on(createdOn);
+    project.setUpdated_on(updatedOn);
+
+    Object ownerIdObj = each.getOrDefault("owner_id", 1);
+    int owner_id;
+    if (ownerIdObj instanceof Long) {
+      owner_id = ((Long) ownerIdObj).intValue();
+    } else if (ownerIdObj instanceof Integer) {
+      owner_id = (Integer) ownerIdObj;
+    } else {
+      owner_id = 1; // Default value
+    }
+
+    project.setOwner_id(owner_id);
+
+    return project;
   }
 }
--- code/pro-manager/src/test/java/com/cs673olsum24/promanager/controller/ProjectControllerTest.java
+++ code/pro-manager/src/test/java/com/cs673olsum24/promanager/controller/ProjectControllerTest.java
@@ -1,109 +1,248 @@
 package com.cs673olsum24.promanager.controller;
 
-import static org.mockito.ArgumentMatchers.any;
-import static org.mockito.Mockito.verify;
-import static org.mockito.Mockito.when;
-import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.delete;
-import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
-import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.jsonPath;
-import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertNotNull;
+import static org.junit.jupiter.api.Assertions.assertTrue;
+import static org.junit.jupiter.api.Assertions.assertFalse;
 
+import com.cs673olsum24.promanager.ProManagerApplication;
 import com.cs673olsum24.promanager.service.ProjectServices;
-import com.fasterxml.jackson.databind.ObjectMapper;
-import jakarta.servlet.http.HttpServletRequest;
+import com.cs673olsum24.promanager.serviceImpl.utils.ProjectFieldIndexes;
+import com.fasterxml.jackson.core.JsonProcessingException;
 import java.util.HashMap;
+import java.util.List;
 import java.util.Map;
+import org.json.simple.JSONObject;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
-import org.springframework.boot.test.mock.mockito.MockBean;
-import org.springframework.http.MediaType;
-import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.ActiveProfiles;
 
-@WebMvcTest(ProjectController.class)
+@SpringBootTest(classes = ProManagerApplication.class)
+@ActiveProfiles("test")
 public class ProjectControllerTest {
 
-  @Autowired private MockMvc mockMvc;
+  @Autowired private ProjectServices projectServices;
 
-  @MockBean private ProjectServices projectServices;
+  private String testProjectId = "project005";
 
-  @Autowired private ObjectMapper objectMapper;
-
-  @Test
-  public void testDeleteProject() throws Exception {
-    Map<String, Object> mockResponse = new HashMap<>();
-    mockResponse.put("Response", "Project deleted successfully");
-    String projectId = "1";
-    when(projectServices.deleteProject(projectId)).thenReturn(mockResponse);
-    mockMvc
-        .perform(
-            delete("/apiv1/project/deleteproject/{id}", projectId)
-                .contentType(MediaType.APPLICATION_JSON))
-        .andExpect(status().isOk())
-        .andExpect(jsonPath("$.Response").value("Project deleted successfully"));
-  }
-
-  @Test
-  public void testAddProject() throws Exception {
-    Map<String, Object> mockResponse = new HashMap<>();
-    mockResponse.put("Response", "OK");
-
+  @BeforeEach
+  public void setup() throws JsonProcessingException {
+    // Prepare and insert a test project
     Map<String, Object> payload = new HashMap<>();
     payload.put(
         "projects",
         new Object[] {
           new HashMap<String, Object>() {
             {
-              put("projectid", "1");
+              put("project_id", testProjectId);
+              put("projectname", "Initial Test Project");
+              put("owner_id", 123);
+              put("description", "Initial description");
+              put("created_on", 1622547800L);
+              put("updated_on", 1622547800L);
+              put("status", "Inactive");
+              put("type", "Initial");
+              put("active", true);
+            }
+          }
+        });
+
+    // Insert the project
+    projectServices.addProject(null, payload);
+  }
+
+  @Test
+  public void testRetrieveProject() {
+    String projectId = testProjectId;
+
+    try {
+      Map<String, Object> response = projectServices.getIdWiseProject(projectId);
+
+      // Assert that the project was found and the response is not null
+      assertNotNull(response);
+      assertNotNull(response.get("project"));
+      List<?> projects = (List<?>) response.get("project");
+      assertFalse(projects.isEmpty());
+
+      // Assuming the project details are in the first object of the array
+      Object[] projectDetails = (Object[]) projects.get(0);
+      assertEquals("Initial Test Project", projectDetails[ProjectFieldIndexes.PROJECT_NAME]);
+      assertEquals("Initial description", projectDetails[ProjectFieldIndexes.DESCRIPTION]);
+      assertEquals("Inactive", projectDetails[ProjectFieldIndexes.STATUS]);
+      assertEquals("Initial", projectDetails[ProjectFieldIndexes.TYPE]);
+    } catch (JsonProcessingException e) {
+      e.printStackTrace();
+      assertNotNull(null, "JsonProcessingException was thrown");
+    }
+  }
+
+  @Test
+  public void testRetrieveNonExistentProject() {
+    String projectId = "nonexistent";
+
+    try {
+      Map<String, Object> response = projectServices.getIdWiseProject(projectId);
+
+      // Assert that the project was not found
+      assertTrue(
+          response.isEmpty()
+              || (response.containsKey("project")
+                  && ((List<?>) response.get("project")).isEmpty()));
+    } catch (JsonProcessingException e) {
+      e.printStackTrace();
+      assertNotNull(null, "JsonProcessingException was thrown");
+    }
+  }
+
+  @Test
+  public void testDeleteProject() {
+    String projectId = testProjectId;
+
+    // Call the service method to delete the project
+    Map<String, Object> response = projectServices.deleteProject(projectId);
+
+    // Assert the response
+    assertNotNull(response);
+    assertEquals("Success", response.get("Response"));
+
+    // Verify that the project is deleted
+    try {
+      Map<String, Object> deletedProjectResponse = projectServices.getIdWiseProject(projectId);
+      assertTrue(
+          deletedProjectResponse.isEmpty()
+              || (deletedProjectResponse.containsKey("project")
+                  && ((List<?>) deletedProjectResponse.get("project")).isEmpty()));
+    } catch (JsonProcessingException e) {
+      e.printStackTrace();
+      assertNotNull(null, "JsonProcessingException was thrown");
+    }
+  }
+
+  @Test
+  public void testAddProject() {
+    // Prepare payload
+    String newProjectId = "proj_006";
+    Map<String, Object> payload = new HashMap<>();
+    payload.put(
+        "projects",
+        new Object[] {
+          new HashMap<String, Object>() {
+            {
+              put("project_id", newProjectId);
               put("projectname", "Test Project");
-              put("userid", "123");
-              put("taskid", 456);
+              put("owner_id", 123);
               put("description", "A test project");
               put("created_on", 1622547800L);
               put("updated_on", 1622547800L);
               put("status", "Active");
               put("type", "Test");
+              put("active", true);
             }
           }
         });
 
-    when(projectServices.addProject(any(HttpServletRequest.class), any(Map.class)))
-        .thenReturn(mockResponse);
+    // Call the service method
+    Map<String, Object> response = projectServices.addProject(null, payload);
 
-    mockMvc
-        .perform(
-            post("/apiv1/project/addprojects/{id}", "1")
-                .contentType(MediaType.APPLICATION_JSON)
-                .content(objectMapper.writeValueAsString(payload)))
-        .andExpect(status().isOk())
-        .andExpect(jsonPath("$.Response").value("OK"));
+    // Assert the response
+    assertNotNull(response);
+    assertEquals("OK", response.get("Response"));
+
+    // Verify that the project was added
+    try {
+      Map<String, Object> addedProjectResponse = projectServices.getIdWiseProject(newProjectId);
+      assertNotNull(addedProjectResponse);
+      assertNotNull(addedProjectResponse.get("project"));
+      List<?> projects = (List<?>) addedProjectResponse.get("project");
+      assertFalse(projects.isEmpty());
+
+      // Assuming the project details are in the first object of the array
+      Object[] projectDetails = (Object[]) projects.get(0);
+      assertEquals("Test Project", projectDetails[ProjectFieldIndexes.PROJECT_NAME]);
+      assertEquals("A test project", projectDetails[ProjectFieldIndexes.DESCRIPTION]);
+      assertEquals("Active", projectDetails[ProjectFieldIndexes.STATUS]);
+      assertEquals("Test", projectDetails[ProjectFieldIndexes.TYPE]);
+    } catch (JsonProcessingException e) {
+      e.printStackTrace();
+      assertNotNull(null, "JsonProcessingException was thrown");
+    }
   }
 
   @Test
-  public void testEditProject() throws Exception {
-    Map<String, Object> mockResponse = new HashMap<>();
-    mockResponse.put("Response", "Success");
-
+  public void testEditProject() {
+    // Prepare payload
     Map<String, Object> payload = new HashMap<>();
-    payload.put("projectid", "project005");
+    payload.put("project_id", testProjectId);
     payload.put("projectname", "Updated Test Project");
-    payload.put("userid", "123");
-    payload.put("taskid", 456);
+    payload.put("owner_id", 123);
     payload.put("description", "An updated description");
     payload.put("updated_on", 1622547800L);
     payload.put("status", "Active");
     payload.put("type", "Test");
 
-    when(projectServices.editProject(any())).thenReturn(mockResponse);
+    // Convert payload to JSONObject
+    JSONObject jsonPayload = new JSONObject(payload);
 
-    mockMvc
-        .perform(
-            post("/apiv1/project/editProject")
-                .contentType(MediaType.APPLICATION_JSON)
-                .content(objectMapper.writeValueAsString(payload)))
-        .andExpect(status().isOk())
-        .andExpect(jsonPath("$.Response").value("Success"));
+    // Call the service method
+    Map<String, Object> response = projectServices.editProject(jsonPayload);
 
-    verify(projectServices).editProject(any());
+    // Assert the response
+    assertNotNull(response);
+    assertEquals("Success", response.get("Response"));
+
+    // Verify that the project details were updated
+    try {
+      Map<String, Object> updatedProjectResponse = projectServices.getIdWiseProject(testProjectId);
+      assertNotNull(updatedProjectResponse);
+      assertNotNull(updatedProjectResponse.get("project"));
+      List<?> projects = (List<?>) updatedProjectResponse.get("project");
+      assertFalse(projects.isEmpty());
+
+      // Assuming the project details are in the first object of the array
+      Object[] projectDetails = (Object[]) projects.get(0);
+      assertEquals("Updated Test Project", projectDetails[ProjectFieldIndexes.PROJECT_NAME]);
+      assertEquals("An updated description", projectDetails[ProjectFieldIndexes.DESCRIPTION]);
+      assertEquals("Active", projectDetails[ProjectFieldIndexes.STATUS]);
+      assertEquals("Test", projectDetails[ProjectFieldIndexes.TYPE]);
+    } catch (JsonProcessingException e) {
+      e.printStackTrace();
+      assertNotNull(null, "JsonProcessingException was thrown");
+    }
+  }
+
+  @Test
+  public void testGetAllProjects() {
+    try {
+      // Use the service to retrieve all projects
+      Map<String, Object> response = projectServices.getAllProjects();
+
+      // Assert that the response is not null and contains the "projects" key
+      assertNotNull(response);
+      assertNotNull(response.get("projects"));
+      assertTrue(response.get("projects") instanceof List);
+
+      // You can further assert specific details about the projects if needed
+      List<?> projects = (List<?>) response.get("projects");
+      assertTrue(!projects.isEmpty(), "Projects list should not be empty");
+
+      // Verify details of the test project
+      Object[] projectDetails =
+          (Object[])
+              projects.stream()
+                  .filter(p -> ((Object[]) p)[ProjectFieldIndexes.PROJECT_ID].equals(testProjectId))
+                  .findFirst()
+                  .orElse(null);
+
+      assertNotNull(projectDetails);
+      assertEquals("Initial Test Project", projectDetails[ProjectFieldIndexes.PROJECT_NAME]);
+      assertEquals("Initial description", projectDetails[ProjectFieldIndexes.DESCRIPTION]);
+      assertEquals("Inactive", projectDetails[ProjectFieldIndexes.STATUS]);
+      assertEquals("Initial", projectDetails[ProjectFieldIndexes.TYPE]);
+    } catch (JsonProcessingException e) {
+      e.printStackTrace();
+      assertNotNull(null, "JsonProcessingException was thrown");
+    }
   }
 }
