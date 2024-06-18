d9fee219cb3199ddae466f4094a6c4e2fe391173
Praveen Singh <80828529+praveensingh3123@users.noreply.github.com>
2024-06-18 17:47:10 +0530

    Update CommentService.java

--- code/pro-manager/src/main/java/com/cs673olsum24/promanager/service/CommentService.java
+++ code/pro-manager/src/main/java/com/cs673olsum24/promanager/service/CommentService.java
@@ -3,7 +3,11 @@ package com.cs673olsum24.promanager.service;
 import com.cs673olsum24.promanager.entity.Comment;
 import com.fasterxml.jackson.core.JsonProcessingException;
 
+import jakarta.servlet.http.HttpServletRequest;
+
 import java.util.Map;
+
+import org.json.simple.JSONObject;
 
 /**
  * Author : Praveen Singh
@@ -33,7 +37,9 @@ public interface CommentService {
      * @return A Map containing the result of the operation. The map typically includes
      *         a success flag, a message, and the newly added comment if the addition is successful.
      */
-    Map<String, Object> addComment(Comment comment);
+//    Map<String, Object> addComment(Comment comment);
+
+	public abstract Map<String, Object>  addComment(HttpServletRequest request, Map<String, Object> payload);
 
     /**
      * Deletes a comment from the system based on its ID.
@@ -47,9 +53,12 @@ public interface CommentService {
     /**
      * Edits an existing comment in the system.
      *
-     * @param comment the Comment object containing the updated data.
+     * @param body the Comment object containing the updated data.
      * @return A Map containing the result of the operation. The map typically includes
      *         a success flag, a message, and the updated comment if the edit is successful.
      */
-    Map<String, Object> editComment(Comment comment);
+    Map<String, Object> editComment(JSONObject body);
+
+
+
 }
