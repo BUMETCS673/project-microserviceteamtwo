a06ab0d900b38f13d72aed4987b26fbbad83411f
Pranjal Ekhande <pranjalekhande11@gmail.com>
2024-06-18 18:50:00 +0530

    Added Edge Test cases
    For REST API (Task GET), Task Delete, Task ADD
    
    Co-Authored-By: AMAN JAIN <98209921+Aman23000@users.noreply.github.com>

--- code/pro-manager/src/test/java/com/cs673olsum24/promanager/controller/TaskControllerTests.java
+++ code/pro-manager/src/test/java/com/cs673olsum24/promanager/controller/TaskControllerTests.java
@@ -16,6 +16,7 @@ import java.util.List;
 import java.util.Map;
 import org.json.simple.JSONObject;
 import org.junit.jupiter.api.BeforeEach;
+
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.MethodOrderer;
 import org.junit.jupiter.api.Order;
@@ -36,16 +37,23 @@ public class TaskControllerTests {
 	   @Autowired private ProjectServices projectServices;
 	   
 
-	   private String testTaskId = "task_001";
+	   private String testTaskId = "task_002";
 	   
 	   private String testProjectId = "proj_002";
 	   
 	   
+//	   @BeforeEach
+//	   public void setupProject() throws JsonProcessingException {
+//	     // Prepare and insert a test project
+//	     
+//	   }
+//	   
 	   @BeforeEach
-	   public void setupProject() throws JsonProcessingException {
-	     // Prepare and insert a test project
-	     Map<String, Object> payload = new HashMap<>();
-	     payload.put(
+	   public void setup() throws JsonProcessingException {
+		   
+		   
+	   Map<String, Object> payload_projects = new HashMap<>();
+	     payload_projects.put(
 	         "projects",
 	         new Object[] {
 	           new HashMap<String, Object>() {
@@ -64,11 +72,9 @@ public class TaskControllerTests {
 	         });
 
 	     // Insert the project
-	     projectServices.addProject(null, payload);
-	   }
-	   
-	   @BeforeEach
-	   public void setup() throws JsonProcessingException {
+	     projectServices.addProject(null, payload_projects);
+	     
+	     
 	     // Prepare and insert a test project
 	     
 		 Map<String, Object> payload = new HashMap<>();
@@ -109,22 +115,25 @@ public class TaskControllerTests {
 	     try {
 	       Map<String, Object> response = taskServices.getAllTasks(projectId);
 	       
-	       System.out.println("proj_001");
-	       System.out.println(response.get("tasks"));
+	      
 	       
 	       // Assert that the project was found and the response is not null
 	       assertNotNull(response);
 	       assertNotNull(response.get("tasks"));
 	       List<?> tasks = (List<?>) response.get("tasks");
 	       assertFalse(tasks.isEmpty());
-	       System.out.println("In Test here");
 
 	       // Assuming the project details are in the first object of the array
-	       Object[] taskDetails = (Object[]) tasks.get(0);
-	       assertEquals("Initial Test Task", taskDetails[TaskFieldIndexes.TASK_NAME]);
-	       assertEquals("Initial description", taskDetails[TaskFieldIndexes.DESCRIPTION]);
-	       assertEquals("To Do", taskDetails[TaskFieldIndexes.STATUS]);
-	       assertEquals("Low", taskDetails[TaskFieldIndexes.PRIORITY]);
+	       
+	       Map<?, ?> taskDetails = (Map<?, ?>) tasks.get(0);
+	      	assertEquals("Initial Test Task", taskDetails.get("task_name"));
+           
+           assertEquals("Initial description", taskDetails.get("description"));
+           assertEquals("To Do", taskDetails.get("status"));
+           assertEquals("Low", taskDetails.get("priority"));
+
+	       
+	     
 	     } catch (JsonProcessingException e) {
 	       e.printStackTrace();
 	       assertNotNull(null, "JsonProcessingException was thrown");
@@ -133,16 +142,14 @@ public class TaskControllerTests {
 	   
 	   @Test
 	   @Order(2)
-	   public void testGetAllProjects() {
+	   public void testGetAllProjectTasks() {
 		   
-		 String projectId = "proj_002";
+	
 	     try {
 	       // Use the service to retrieve all projects
-	       Map<String, Object> response = taskServices.getAllTasks(projectId);
+	       Map<String, Object> response = taskServices.getAllTasks(testProjectId);
 	       
-	       System.out.print("proj_001");
-	       System.out.print(response.get("tasks"));
-
+	      
 	       // Assert that the response is not null and contains the "projects" key
 	       assertNotNull(response);
 	       assertNotNull(response.get("tasks"));
@@ -151,25 +158,145 @@ public class TaskControllerTests {
 	       // You can further assert specific details about the projects if needed
 	       List<?> tasks = (List<?>) response.get("tasks");
 	       assertTrue(!tasks.isEmpty(), "Tasks list should not be empty");
+	       
+	     
+	       // Verify details of the test tasks
+	        Map<?, ?> taskDetails = tasks.stream()
+	            .filter(p -> ((Map<?, ?>) p).get("task_id").equals(testTaskId))
+	            .map(p -> (Map<?, ?>) p)
+	            .findFirst()
+	            .orElse(null);
+	        
 
-	       // Verify details of the test Tasks
-	       Object[] taskDetails =
-	    		   		(Object[])
-	    		   		tasks.stream()
-	                   .filter(p -> ((Object[]) p)[TaskFieldIndexes.TASK_ID].equals(testTaskId))
-	                   .findFirst()
-	                   .orElse(null);
+	        assertNotNull(taskDetails);
+	        assertEquals("Initial Test Task", taskDetails.get("task_name"));
+	        assertEquals("Initial description", taskDetails.get("description"));
+	        assertEquals("To Do", taskDetails.get("status"));
+	        assertEquals("Low", taskDetails.get("priority"));
 
-	       assertNotNull(taskDetails);
-	       assertEquals("Initial Test Task", taskDetails[TaskFieldIndexes.TASK_NAME]);
-	       assertEquals("Initial Test Project Task", taskDetails[TaskFieldIndexes.DESCRIPTION]);
-	       assertEquals("To Do", taskDetails[TaskFieldIndexes.STATUS]);
-	       assertEquals("Low", taskDetails[TaskFieldIndexes.PRIORITY]);
+	        
 	     } catch (JsonProcessingException e) {
 	       e.printStackTrace();
 	       assertNotNull(null, "JsonProcessingException was thrown");
 	     }
 	   }
+	   
+	   @Test
+	   @Order(3)
+	   public void testRetrieveNonExistentProject() {
+	     String projectId = "nonexistent";
+
+	     try {
+	       Map<String, Object> response = taskServices.getAllTasks(projectId);
+	       
+	       assertNotNull(response, "Response should not be null");
+	       
+	       boolean noTasksFound = response.isEmpty() ||
+                   (response.containsKey("tasks") && ((List<?>) response.get("tasks")).isEmpty());
+	       assertTrue(noTasksFound, "The response should be empty or contain an empty 'tasks' list");
+
+	    
+	     } catch (JsonProcessingException e) {
+	       e.printStackTrace();
+	       assertNotNull(null, "JsonProcessingException was thrown");
+	     }
+	   }
+	  
+	   @Test
+	   @Order(4)
+	   public void testDeleteProject() {
+		   
+		   String projectId = testProjectId;
+	     String taskId = testTaskId;
+
+	     // Call the service method to delete the project
+	     Map<String, Object> response = taskServices.deleteTask(taskId);
+
+	     // Assert the response
+	     assertNotNull(response);
+	     assertEquals("Success", response.get("Response"));
+
+	     // Verify that the project is deleted
+	     try {
+	       Map<String, Object> deletedProjectResponse = taskServices.getAllTasks(projectId);
+	       assertTrue(
+	           deletedProjectResponse.isEmpty()
+	               || (deletedProjectResponse.containsKey("tasks")
+	                   && ((List<?>) deletedProjectResponse.get("tasks")).isEmpty()));
+	     } catch (JsonProcessingException e) {
+	       e.printStackTrace();
+	       assertNotNull(null, "JsonProcessingException was thrown");
+	     }
+	   }
+	   
+
+	   @Test
+	   @Order(5)
+	   public void testAddTasks() {
+	     // Prepare payload
+		   
+		   String newProjectId = "proj_002";
+		   
+	     String newProjectTaskId = "task_003";
+	     Map<String, Object> payload = new HashMap<>();
+	     
+	     
+	     payload.put(
+		         "tasks",
+		         new Object[] {
+		           new HashMap<String, Object>() {
+		             {
+		               put("task_id", newProjectTaskId);
+		               put("project_id", newProjectId);
+		               put("task_name", "Added Test Task");	             
+		               put("description", "Initial description");
+		               put("status", "To Do");
+		              
+		               put("priority", "Low");	               
+		              
+		              
+		               put("assigned_user_id", 1);
+		               put("due_date", 1717941368L);
+		               put("created_on", 1717941368L);
+		               put("updated_on", 1717941368L);
+		             }
+		           }
+		         });
+
+	     // Call the service method
+	     
+	     Map<String, Object> response = taskServices.addTask(null, payload);
+
+	     // Assert the response
+	     assertNotNull(response);
+	     assertEquals("OK", response.get("Response"));
+
+	     // Verify that the project was added
+	     try {
+	       Map<String, Object> addedProjectResponse = projectServices.getIdWiseProject(newProjectId);
+	       Map<String, Object> addedTaskResponse = taskServices.getAllTasks(newProjectId);
+	       assertNotNull(addedTaskResponse);
+	       assertNotNull(addedTaskResponse.get("tasks"));
+	       List<?> tasks = (List<?>) addedTaskResponse.get("tasks");
+	       assertFalse(tasks.isEmpty());
+
+	       // Assuming the project details are in the first object of the array
+	       
+	       Map<?, ?> taskDetails = (Map<?, ?>) tasks.get(1);
+	       assertEquals("Added Test Task", taskDetails.get("task_name"));
+           
+             assertEquals("Initial description", taskDetails.get("description"));
+             assertEquals("To Do", taskDetails.get("status"));
+             assertEquals("Low", taskDetails.get("priority"));
+	     } catch (JsonProcessingException e) {
+	       e.printStackTrace();
+	       assertNotNull(null, "JsonProcessingException was thrown");
+	     }
+	   }
+	  
+	
+	   
+
 	   
 
 }
