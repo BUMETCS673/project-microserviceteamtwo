70c3fe1e4889675d72c2b5781fac9fe9c760a5c4
mkljngd <37976753+mkljngd@users.noreply.github.com>
2024-06-19 00:18:47 +0530

    Improved Project Controller test cases and Service Implementation

---
+++ code/pro-manager/src/main/java/com/cs673olsum24/promanager/utils/ProjectFieldIndexes.java
@@ -0,0 +1,13 @@
+package com.cs673olsum24.promanager.utils;
+
+public class ProjectFieldIndexes {
+  public static final int PROJECT_ID = 0;
+  public static final int PROJECT_NAME = 1;
+  public static final int OWNER_ID = 2;
+  public static final int DESCRIPTION = 4;
+  public static final int CREATED_ON = 5;
+  public static final int UPDATED_ON = 6;
+  public static final int STATUS = 7;
+  public static final int TYPE = 8;
+  public static final int ACTIVE = 3;
+}
